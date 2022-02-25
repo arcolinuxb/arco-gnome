30abb30f3b41e71a080a42e90aad2338ce37c3b7
Erik Dubois <erik.dubois@gmail.com>
2022-02-25 18:55:57 +0100

    calamares 52

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="gnome"
 	dmDesktop="gnome-xorg"
 
-	arcolinuxVersion='v22.03.06'
+	arcolinuxVersion='v22.03.07'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
