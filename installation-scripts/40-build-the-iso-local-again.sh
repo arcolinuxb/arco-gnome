08afa7c0023b806b064b21b3e046f25ce8880e6e
Erik Dubois <erik.dubois@gmail.com>
2022-03-05 07:53:11 +0100

    new calamares

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="gnome"
 	dmDesktop="gnome-xorg"
 
-	arcolinuxVersion='v22.03.07'
+	arcolinuxVersion='v22.03.08'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
