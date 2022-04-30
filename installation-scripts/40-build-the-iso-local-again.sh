91eeaa46f813699c1b1e103536ba2f7a3cec7593
ArcoLinux <arcolinuxinfo@gmail.com>
2022-04-30 10:19:10 +0200

    bump

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="gnome"
 	dmDesktop="gnome-xorg"
 
-	arcolinuxVersion='v22.05.02'
+	arcolinuxVersion='v22.06.01'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
