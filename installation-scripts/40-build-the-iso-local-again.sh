0c25ea981f3d1cf66a016d80ace7a2362509abe3
arcolinuxz <arcolinuxinfo@gmail.com>
2023-05-31 12:51:32 +0200

    archiso 71

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="gnome"
 	dmDesktop="gnome-xorg"
 
-	arcolinuxVersion='v23.06.02'
+	arcolinuxVersion='v23.07.01'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
