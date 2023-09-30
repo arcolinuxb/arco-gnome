8da9a5a7b48db5d0410bb37b4c2f65a67a9d19b1
Erik Dubois <erik.dubois@gmail.com>
2023-09-30 06:17:22 +0200

    bump

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="gnome"
 	dmDesktop="gnome-xorg"
 
-	arcolinuxVersion='v23.10.01'
+	arcolinuxVersion='v23.11.01'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
