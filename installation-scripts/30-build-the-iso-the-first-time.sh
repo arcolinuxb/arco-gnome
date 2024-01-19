a66b3fdf6b46853538715996ead4337b7d7cff83
Erik Dubois <erik.dubois@gmail.com>
2024-01-19 19:24:26 +0100

    archiso - personal build

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="gnome"
 	dmDesktop="gnome-xorg"
 
-	arcolinuxVersion='v24.02.03'
+	arcolinuxVersion='v24.02.04'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
